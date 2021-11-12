94a3e1b54e7560f71f73dcb59988fb5b8239cae5
Christian Lachapelle <devworks8@gmail.com>
2021-11-12 10:24:47 -0500

    Initial commit

---
+++ PubVar.py
@@ -0,0 +1,8 @@
+# API specific variables
+VERSION = '1.0'
+TITLE = 'Literary Database'
+DESCRIPTION = 'XML database frontend API using GoogleBook API'
+
+# Namespace specific variables
+NSNAME = 'books'
+NSDESCRIPTION = 'Book related operations'
---
+++ main.py
@@ -0,0 +1,5 @@
+import FlaskAPI.API
+
+if __name__ == '__main__':
+    FlaskAPI.API.app.run(debug=True)
+

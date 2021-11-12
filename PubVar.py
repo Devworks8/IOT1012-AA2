9e0744b6d8be7a274ecd9d8defd107518e590253
Christian Lachapelle <devworks8@gmail.com>
2021-11-12 11:30:06 -0500

    ngrok info

--- PubVar.py
+++ PubVar.py
@@ -10,3 +10,5 @@ NSDESCRIPTION = 'Book related operations'
 API = "https://www.googleapis.com/books/v1/volumes"
 ISBNQUERY = "?q=isbn:"
 TITLEQUERY = "?q=title:"
+
+ngrok = "ngrok authtoken 20pH6Do6sGoifi1a5zXytf3ur8A_77HMAUNvxFxd8vVo14do9"
